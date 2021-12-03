bbdbdf92a7964c9fd39fc44fc8a21d8a829bfdbe
wangruiyu <wangruiyu@bhky.net.cn>
2021-12-03 17:27:32 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -1,16 +1,8 @@
 @echo off
-
 cd /d "%~dp0"
-
-::# 生成静态文件
 call yarn  build
-
-::# 进入生成的文件夹
 cd dist
-
-::
 call git init
 call git add -A
 call git commit -m 'deploy'
-
 call git push -f git@github.com:Post-90sBadKid/blog.git master:main
