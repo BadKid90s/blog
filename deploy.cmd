5ecb4ec253195f1ff7e7364746c471d688b4b2aa
wangruiyu <wangruiyu@bhky.net.cn>
2021-12-03 17:24:06 +0800

    'deploy'

--- deploy.cmd
+++ deploy.cmd
@@ -3,14 +3,14 @@
 cd /d "%~dp0"
 
 ::# 生成静态文件
-yarn  build
+call yarn  build
 
 ::# 进入生成的文件夹
 cd dist
 
 ::
-git init
-git add -A
-git commit -m 'deploy'
+call git init
+call git add -A
+call git commit -m 'deploy'
 
-git push -f git@github.com:Post-90sBadKid/blog.git master:main
+call git push -f git@github.com:Post-90sBadKid/blog.git master:main
