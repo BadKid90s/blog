0545ebde4f9680b6048bc17d967fe36880636739
王瑞玉 <wangruiyu@bjknrt.com>
2023-06-05 11:31:15 +0800

    refactor: 修改部署脚本

--- deploy.bat
+++ deploy.bat
@@ -5,4 +5,4 @@ cd dist
 call git init
 call git add -A
 call git commit -m 'deploy'
-call git push -f https://github.com/BadKid90s/blog.git main:main
+call git push -f https://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@github.com/BadKid90s/blog.git main:page
--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/BadKid90s/blog.git main:main
+git push -f https://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@github.com/BadKid90s/blog.git main:page
+
 
 cd -
